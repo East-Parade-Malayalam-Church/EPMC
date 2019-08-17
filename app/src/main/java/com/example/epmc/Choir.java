d6830ae04c9f74fee2883615e22fe92e15ce59f9
jee14k <jeevan.koshy@science.christuniversity.in>
2019-08-17 15:14:37 +0530

    tried with firebase again and made a few more changes

--- app/src/main/java/com/example/epmc/Choir.java
+++ app/src/main/java/com/example/epmc/Choir.java
@@ -1,13 +1,23 @@
 package com.example.epmc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Choir extends AppCompatActivity {
+    Button chg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choir);
+        chg = (Button) findViewById(R.id.button26);
+    }
+    public void btchg(View view)
+    {
+        Intent i = new Intent(this,Choirgoals.class);
+        startActivity(i);
     }
 }
