7dba986b4d8d97d9923b8bf9269a007f97130bc7
jee14k <jeevan.koshy@science.christuniversity.in>
2019-08-19 10:51:58 +0530

    Made more changes, try getting firebase to work pls. Or start working on the creative part of the app with the help of material design and what not

--- app/src/main/java/com/example/epmc/History.java
+++ app/src/main/java/com/example/epmc/History.java
@@ -2,12 +2,17 @@ package com.example.epmc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class History extends AppCompatActivity {
+    TextView oj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-    }
+        oj = (TextView) findViewById(R.id.textView140);
+        oj.setMovementMethod(new ScrollingMovementMethod());
+        }
 }
