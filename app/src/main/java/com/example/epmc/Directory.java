976139aa719ffb496def352e0e2c75cfabbf2113
jee14k <jeevan.koshy@science.christuniversity.in>
2019-10-15 17:11:16 +0530

    Long time commit during end sem

--- app/src/main/java/com/example/epmc/Directory.java
+++ app/src/main/java/com/example/epmc/Directory.java
@@ -1,15 +1,8 @@
 package com.example.epmc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.Network;
-import android.net.NetworkInfo;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ToggleButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -19,28 +12,7 @@ public class Directory extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        final ToggleButton wifitoggle;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directory);
-        wifitoggle = (ToggleButton) findViewById(R.id.wifitoggle);
-        wifitoggle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(wifitoggle.isChecked())
-                {
-                    WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-                    wifi.setWifiEnabled(true);
-                }
-                else {
-                    WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-                    wifi.setWifiEnabled(false);
-                }
-            }
-        });
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo mWifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        if (mWifi.isConnected()) {
-            wifitoggle.setChecked(true);
-        }
     }
 }
