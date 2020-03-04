3d98b212b71adb448b76c192bb6f6f5b1bca7d5f
jee14k <jeevan.koshy@science.christuniversity.in>
2020-03-04 20:31:07 +0530

    Vers1.0

--- app/src/main/java/com/example/epmc/Directory.java
+++ app/src/main/java/com/example/epmc/Directory.java
@@ -4,11 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Directory extends AppCompatActivity {
-    // Access a Cloud Firestore instance from your Activity
-    public static FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
